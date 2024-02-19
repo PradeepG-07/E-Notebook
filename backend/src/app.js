918fb62aa44cb7afc790938ef3e4603bea65bed0
Pradeep <pradeepubuntuvps@gmail.com>
2024-02-20 00:22:25 +0530

    add get '/' route to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,9 @@ const app = express();
 const port = process.env.PORT || 4000;
 app.use(express.json());
 
-
+app.get("/",(req,res)=>{
+    res.status(200).send("Server Running");
+})
 app.use('/api/auth',authRoutes);
 app.use("/api/notes",notesRoutes);
 export default app;
